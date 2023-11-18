cb858582d3b3a5f1cc00bca97ba972b8d84cfb23
Nyame100 <nyamerichmond100@gmail.com>
2023-11-18 13:18:09 +0100

    change files and folders structure

---
+++ app/page.tsx
@@ -0,0 +1,33 @@
+"use client";
+import { useRouter } from "next/navigation";
+import Navbar from "./(dashboard)/_components/navbar";
+import { Sidebar } from "lucide-react";
+import NavbarRoutes from "@/components/navbar-routes";
+
+const HomePage = () => {
+  const router = useRouter();
+
+  const onClick = () => {
+    router.push("/dashboard");
+  };
+  return (
+    <div className="h-full">
+      {/* Navbar div -- slightly different from general navbar*/}
+      <div className="h-[80px] md:pl-0 fixed inset-y-0 w-full z-50">
+        <div className="p-4 border-b h-full flex items-center bg-white shadow-sm">
+          <NavbarRoutes />
+        </div>
+      </div>
+      {/* Sidebar div */}
+      {/* <div className="hidden md:flex h-full w-56 flex-col fixed inset-y-0 z-50">
+        <Sidebar />
+      </div> */}
+    </div>
+    // <div>
+    //   <div>This is the home page</div>
+    //   <button onClick={onClick}>Click me</button>
+    // </div>
+  );
+};
+
+export default HomePage;
